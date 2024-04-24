81f5d0b7d3320cbee748f69a1b1da7189002885b
Jorge <Jorge@DESKTOP-5EI151T.home>
2024-04-24 20:35:52 +0200

    Commit de comentario en la clase CalculadoraNetBeans

---
+++ src/main/java/com/mycompany/calculadoranetbeans/CalculadoraNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoranetbeans;
+
+/**
+ *
+ * @author Jorge
+ */
+public class CalculadoraNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la primera modificación del repositorio Calculadora.");
+    }
+}
